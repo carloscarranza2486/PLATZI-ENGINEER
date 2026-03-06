6a3b73530231705ebfe7da9994c008b5536b22a4
Carlos Carranza <carloscarranza2486@gmail.com>
2026-03-06 15:53:26 -0500

    feat: add f-strings examples for greeting and age calculation

---
+++ intermediate_course/1_pythonic_coding/f-strings.py
@@ -0,0 +1,14 @@
+name = "Ana"
+year = 2020
+text = f"Hello, {name}"
+text_2 = "Hello"
+
+text_calculo = f"Hola, {name}, tu edad es: {2025 - year} años"
+print(text_calculo)
+
+text_func = f"Hola {name.upper()}"
+print(text_func)
+
+edad = 16
+text_if = f"hola {name}, eres {'mayor' if edad > 18 else 'menor'} de edad"
+print(text_if)
