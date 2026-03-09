5e77828922e2dcf2478ba8a9bc97de1e1bd830e3
Carlos Carranza <carloscarranza2486@gmail.com>
2026-03-09 18:03:14 -0500

    feat: add type hints example in type-hints.py

---
+++ intermediate_course/3_data_management/type-hints.py
@@ -0,0 +1,15 @@
+"""Typing con Python"""
+
+variable = 42  # int
+print(f"Variable {variable}, del tipo {type(variable)}")
+
+variable = "Texto de prueba"
+print(f"Variable {variable}, del tipo {type(variable)}")
+
+otra_variable: int = 44
+print(f"Variable {otra_variable}, del tipo {type(otra_variable)}")
+
+otra_variable = ""
+
+
+user_id: int | None = None
