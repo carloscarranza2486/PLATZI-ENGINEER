7870f9eec7c6dd4ab37ec36d3cf2495fd64e440d
Carlos Carranza <carloscarranza2486@gmail.com>
2026-03-09 16:48:41 -0500

    feat: refactor newsapi_client to return JSON data and update fetch_news function

--- intermediate_course/1_pythonic_coding/main.py
+++ intermediate_course/1_pythonic_coding/main.py
@@ -44,20 +44,9 @@
 # # Líneas en blanco: Separar funciones y clases • lógicamente
 # # • Comillas consistentes: Usar comillas dobles para strings
 
-import urllib.request
+import json
 import urllib.parse
-API_KEY = "302b596673314c03a740f3c64b2f1ff6"
-BASE_URL = "https://newsapi.org/v2/everything"
-
-
-def newsapi_client(api_key, query, timeout=30, retries=3):
-    query_string = urllib.parse.urlencode({"q": query, "apiKEY": api_key})
-    url = f"{BASE_URL}?{query_string}"
-    with urllib.request.urlopen(url, timeout=timeout) as response:
-        data = response.read()
-        # Print first 100 chars for brevity
-        print(f"Response data: {data[:100]}...")
-    return f"NewAPI: {query} con timeout {timeout}"
+import urllib.request
 
 
 def guardian_client(api_key, section, from_date, timeout=30, retries=3):
@@ -81,19 +70,32 @@ def ejemplo_kwargs(**kwargs):
     print("====")
 
 
-ejemplo_kwargs(
-    api_key="DEMO",
-    query="Noticias de python",
-    timeout=30,
-    retries=3,
-)
-ejemplo_kwargs(
-    api_key="DEMO GUARDIAN",
-    section="Sports",
-    from_date="2020-10-20",
-    timeout=30,
-    retries=3
-)
+# ejemplo_kwargs(
+#     api_key="DEMO",
+#     query="Noticias de python",
+#     timeout=30,
+#     retries=3,
+# )
+# ejemplo_kwargs(
+#     api_key="DEMO GUARDIAN",
+#     section="Sports",
+#     from_date="2020-10-20",
+#     timeout=30,
+#     retries=3
+# )
+
+API_KEY = "302b596673314c03a740f3c64b2f1ff6"
+BASE_URL = "https://newsapi.org/v2/everything"
+
+
+def newsapi_client(api_key, query, timeout=30, retries=3):
+    query_string = urllib.parse.urlencode({"q": query, "apiKEY": api_key})
+    url = f"{BASE_URL}?{query_string}"
+
+    with urllib.request.urlopen(url, timeout=timeout) as response:
+        data = response.read().decode("utf-8")
+        return json.loads(data)
+    return f"NewAPI: {query} con timeout {timeout}"
 
 
 def fetch_news(api_name, *args, **kwargs):
@@ -105,3 +107,21 @@ def fetch_news(api_name, *args, **kwargs):
         "timeout": 30,
         "retries": 3,
     }
+
+    config = {
+        **base_config,
+        **kwargs
+    }
+
+    api_client = {
+        "newapi": newsapi_client,
+        "guardian": guardian_client,
+    }
+
+    client = api_client[api_name]
+    return client(*args, **config)
+
+
+response_data = fetch_news("newapi", api_key=API_KEY, query="Python")
+for article in response_data["articles"]:
+    print(article["title"])
