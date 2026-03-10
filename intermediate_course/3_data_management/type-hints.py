7c37428a354662382e626c640c46ad1f7280aff1
Carlos Carranza <carloscarranza2486@gmail.com>
2026-03-09 20:20:35 -0500

    feat: add type hints for variables and functions in type-hints.py

--- intermediate_course/3_data_management/type-hints.py
+++ intermediate_course/3_data_management/type-hints.py
@@ -1,5 +1,7 @@
 """Typing con Python"""
 
+from typing import Any
+
 variable = 42  # int
 print(f"Variable {variable}, del tipo {type(variable)}")
 
@@ -13,3 +15,19 @@ otra_variable = ""
 
 
 user_id: int | None = None
+
+
+def suma_clara(a: int, b: int) -> int:
+    return a + b
+
+
+articles: list[dict] = [{"title": "Example"}, {"title": "Example 2"}]
+articles_dos: list[list[str]] = [
+    ["articulos", "otros", 123],
+    ["articulos", "otros"],
+]
+
+articles_tres: list[list[Any]] = [
+    ["articulos", "otros"],
+    ["articulos", "otros"],
+]
