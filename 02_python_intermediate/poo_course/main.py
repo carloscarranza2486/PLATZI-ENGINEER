08fe9b705db8b6845d711f14dc5356f6a7ba9ef4
Carlos Carranza <carloscarranza2486@gmail.com>
2026-03-24 22:02:24 -0500

    feat: added classes for books and users

--- 02_python_intermediate/poo_course/main.py
+++ 02_python_intermediate/poo_course/main.py
@@ -1,3 +1,20 @@
+from typing import Protocol
+
+
+class LibroProtocol(Protocol):
+    def prestar(self) -> str:
+        """Método de prestar un libro"""
+        ...
+
+    def devolver(self) -> str:
+        """Método de devolver un libro"""
+        ...
+
+    def calcular_duracion(self) -> str:
+        """Calcula el tiempo de prestamo"""
+        ...
+
+
 class Libro:
     def __init__(self, titulo, autor, isbn, disponible=True):
         self.titulo = titulo
@@ -6,16 +23,19 @@ class Libro:
         self.disponible = disponible
         self.__veces_prestado = 0
 
+    def __str__(self):
+        return f"{self.titulo} por {self.autor} disponible: {self.disponible}"
+
     def prestar(self):
         if self.disponible:
             self.disponible = False
-            self.__veces_prestado = +1
-            return f"{self.titulo}: prestado exitosamente. Total préstamos: {self.__veces_prestado}"
+            self.__veces_prestado += 1
+            return f"'{self.titulo}' prestado exitosamente. Total préstamos: {self.__veces_prestado}"
         return f"'{self.titulo}' no está disponible"
 
     def devolver(self):
         self.disponible = True
-        return f"{self.titulo}: ha sido devuelto y disponible nuevamente."
+        return f"'{self.titulo}' devuelto y disponible nuevamente"
 
     def es_popular(self):
         return self.__veces_prestado > 5
@@ -23,19 +43,49 @@ class Libro:
     def get_veces_prestado(self):
         return self.__veces_prestado
 
-    def set_veces_prestado(self, veces_prestadas):
-        self.__veces_prestado = veces_prestadas
+    def set_veces_prestado(self, veces_prestado):
+        self.__veces_prestado = veces_prestado
 
 
-mi_libro = Libro("100 años de soledad",
-                 "Gabriel García Marquez", "9781644734728", True)
-otro_libro = Libro("El Principito", "Saint-Exupéry", "9781644731234728", True)
-
-mi_libro.set_veces_prestado(10)
-print(mi_libro.get_veces_prestado())
+class LibroFisico(Libro):
+    def calcular_duracion(self):
+        return "7 días"
 
 
-catalogo = [mi_libro, otro_libro]
+class LibroDigital(Libro):
+    def calcular_duracion(self):
+        return "14 días"
 
-for libro in catalogo:
-    print(libro)
+
+class Biblioteca:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.libros = []
+        self.usuarios = []
+
+    def libros_disponibles(self):
+        return [libro.titulo for libro in self.libros if libro.disponible]
+
+
+mi_libro = LibroFisico(
+    "100 Años de Soledad",
+    "Gabriel Garcia Marquez",
+    "9781644734728",
+    True,
+)
+mi_libro_no_disponible = LibroFisico(
+    "No disponible",
+    "Luis",
+    "56789",
+    True,
+)
+otro_libro = LibroFisico(
+    "El Principito",
+    "Saint-Exupéry",
+    "9781644731234728",
+    True,
+)
+
+biblioteca = Biblioteca("Platzi Biblioteca")
+biblioteca.libros = [mi_libro, mi_libro_no_disponible, otro_libro]
+print(biblioteca.libros_disponibles())
